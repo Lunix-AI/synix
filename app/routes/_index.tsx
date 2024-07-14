e01190e744b306042786dc7c7ad6d01189ed533c
firtoz <108406948+firtoz@users.noreply.github.com>
2024-07-14 19:22:33 +0800

    YJS synced tldraw?

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,88 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useMemo } from "react";
+import {
+	type TLUiOverrides,
+	Tldraw,
+	StateNode,
+	type TLEventHandlers,
+	useEditor,
+	track,
+} from "tldraw";
+import "tldraw/tldraw.css";
+import { useYjsStore } from "../store/useYjsStore";
+
+const HOST_URL =
+	import.meta.env.MODE === "development"
+		? "ws://localhost:1234"
+		: "wss://demos.yjs.dev";
+
+function YjsExample() {
+	const store = useYjsStore({
+		roomId: "example17",
+		hostUrl: HOST_URL,
+	});
+
+	return (
+		<Tldraw
+			autoFocus
+			store={store}
+			components={{
+				SharePanel: NameEditor,
+			}}
+		/>
+	);
+}
+
+const NameEditor = track(() => {
+	const editor = useEditor();
+
+	const { color, name } = editor.user.getUserPreferences();
+
+	return (
+		<div style={{ pointerEvents: "all", display: "flex" }}>
+			<input
+				type="color"
+				value={color}
+				onChange={(e) => {
+					editor.user.updateUserPreferences({
+						color: e.currentTarget.value,
+					});
+				}}
+			/>
+			<input
+				value={name}
+				onChange={(e) => {
+					editor.user.updateUserPreferences({
+						name: e.currentTarget.value,
+					});
+				}}
+			/>
+		</div>
+	);
+});
 
 export const meta: MetaFunction = () => {
 	return [
-		{ title: "New Remix App" },
-		{ name: "description", content: "Welcome to Remix!" },
+		{ title: "Synix AI" },
+		{
+			name: "description",
+			content: "Non-linear conversations right here in your browser.",
+		},
 	];
 };
 
+class Card extends StateNode {
+	static override id = "card";
+
+	onPointerDown: TLEventHandlers["onPointerDown"] = (info) => {
+		console.log("hello", info);
+	};
+}
+
 export default function Index() {
 	return (
-		<div className="font-sans p-4">
-			<h1 className="text-3xl">Welcome to Remix</h1>
-			<ul className="list-disc mt-4 pl-6 space-y-2">
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/start/quickstart"
-						rel="noreferrer"
-					>
-						5m Quick Start
-					</a>
-				</li>
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/start/tutorial"
-						rel="noreferrer"
-					>
-						30m Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/docs"
-						rel="noreferrer"
-					>
-						Remix Docs
-					</a>
-				</li>
-			</ul>
+		<div style={{ position: "fixed", inset: 0 }}>
+			<YjsExample />
 		</div>
 	);
 }
